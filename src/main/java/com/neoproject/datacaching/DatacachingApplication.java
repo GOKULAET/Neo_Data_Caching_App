a45e336b708a27e44f8d21d82aca75bdb323549a
Raj <gokul.raj@nokia.com>
2019-12-02 20:22:50 +0530

    Initail Setup

---
+++ src/main/java/com/neoproject/datacaching/DatacachingApplication.java
@@ -0,0 +1,13 @@
+package com.neoproject.datacaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoproject/datacaching/DatacachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoproject.datacaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatacachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
